0ab3fb295e95fdd037b6cd35598546ca245ca92e
sreesandhyagit <sreesandhyaajesh@gmail.com>
2025-10-06 20:36:30 +0530

    updates..

---
+++ JavaScript/nodejs.js
@@ -0,0 +1,10 @@
+console.log("Hello")
+let s = "Sree"
+var n ="Hello"
+const age = 18
+let numbers = [12,67,2,4]
+let student={name:"joyal",age:22,course:"python"}
+console.log(s,n,age)
+console.log(numbers[1])
+console.log(student['name'])
+console.log(student.age)
